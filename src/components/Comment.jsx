73fb66ca0fffc43029439f71025279bad7573751
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2021-12-14 22:09:33 +0000

    Projeto Criado e Comment component criado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import "./Comment.css";
+
+const Comment = () => {
+  return (
+    <div className="comment">
+      <h2>Jonathan Melo</h2>
+      <p>O jonathan é muito lindão.</p>
+      <p>2021-20-12</p>
+    </div>
+  );
+};
+
+export default Comment;
