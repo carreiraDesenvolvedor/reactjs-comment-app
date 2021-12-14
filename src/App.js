5c8065e4efc4ca43cfca0865cb02d4dab06f18be
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2021-12-14 22:42:18 +0000

    Passando propriedades para o Comment component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ function App() {
   return (
     <div className="App">
       <h1>My Comments App!</h1>
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
+      <Comment
+        name="Jonathan Melo"
+        email="desenvolvedor.jonathan@gmail.com"
+        date={new Date()}
+        message="O jonathan é muito lindão."
+      />
+      <Comment
+        name="Jackeline Gois"
+        email="j@gmail.com"
+        date={new Date()}
+        message="Realmente o jonathan é muito lindão."
+      />
     </div>
   );
 }
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,14 @@
 import "./Comment.css";
 
-const Comment = () => {
+const Comment = (props) => {
+  const { name, email, message, date } = props;
+
   return (
     <div className="comment">
-      <h2>Jonathan Melo</h2>
-      <p>O jonathan é muito lindão.</p>
-      <p>2021-20-12</p>
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <p>{message}</p>
+      <p>{date.toString()}</p>
     </div>
   );
 };
